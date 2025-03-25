afc7204064193e2a46a208a3dbd6b7aa655e2ae9
hieuvx <voxuanhieu_t64@hus.edu.vn>
2025-03-25 14:18:17 +0700

    fix cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','https://film-saga-manager.vercel.app/*'],
 
     'allowed_methods' => ['*'],
 
